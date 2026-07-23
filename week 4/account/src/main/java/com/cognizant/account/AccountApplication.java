0e35c75161138900d57a452fa511f959104a72bb
sandeep-6666 <sandeepchebrolu6@gmail.com>
2026-07-23 20:20:40 +0530

    adding week4

--- week 4/account/src/main/java/com/cognizant/account/AccountApplication.java
+++ week 4/account/src/main/java/com/cognizant/account/AccountApplication.java
@@ -2,7 +2,10 @@ package com.cognizant.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// @SpringBootApplication is a convenience annotation that adds all of the following:
+// @Configuration: Tags the class as a source of bean definitions for the application context.
+// @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings
+// @ComponentScan: Tells Spring to look for other components, configurations, and services in the com/cognizant/account package, allowing it to find the controllers.
 @SpringBootApplication
 public class AccountApplication {
 
