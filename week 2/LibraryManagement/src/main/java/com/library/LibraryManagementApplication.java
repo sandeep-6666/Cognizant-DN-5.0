6f9c5b1739b913a6fff0da5ae9d7e6449439161a
sandeep-6666 <sandeepchebrolu6@gmail.com>
2026-07-23 20:16:42 +0530

    LibraryManagment

--- week 2/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
+++ week 2/LibraryManagement/src/main/java/com/library/LibraryManagementApplication.java
@@ -9,13 +9,11 @@ public class LibraryManagementApplication {
     public static void main(String[] args) {
         System.out.println("=== Loading Spring Application Context ===");
 
-        // Load beans defined in applicationContext.xml
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
         System.out.println("=== Context loaded. Retrieving beans ===");
 
-        // Get the BookService bean - Spring has already injected BookRepository into it
         BookService bookService = context.getBean("bookService", BookService.class);
 
         System.out.println("=== Testing Dependency Injection ===");
